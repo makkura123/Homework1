81700eee91b781219bdd4f14fe2f25278cd05646
nammon16 <nammon16@my.whitworth.edu>
2015-01-06 14:27:32 -0800

    Exercise 4

--- AreaOfCircle/src/areaofcircle/AreaOfCircle.java
+++ AreaOfCircle/src/areaofcircle/AreaOfCircle.java
@@ -1,26 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Calculate the area of a circle
+ * @author Neele Ammon
  */
 package areaofcircle;
 import java.util.Scanner;
 
-/**
- *
- * @author Neele
- */
 public class AreaOfCircle {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+         * Prompts user for radius input
+         */
+        System.out.println("What is the radius of your circle? ");
         Scanner input = new Scanner(System.in);
         int radius = (int) input.nextFloat();
+        
+        /** 
+         * Assign 3.14 as a float to variable PI
+         */
         final float PI = 3.14f;
         
+        /**
+         * Calculates the area of the circle and prints the result
+         */
         System.out.printf("The radius is %f", PI*radius*radius);
     }
     
