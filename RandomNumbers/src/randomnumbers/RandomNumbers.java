3d0919fe7cc86ef43508b95acc24aa9bb92dbd36
nammon16 <nammon16@my.whitworth.edu>
2015-01-07 07:53:25 -0800

    PR1_5 - done

--- RandomNumbers/src/randomnumbers/RandomNumbers.java
+++ RandomNumbers/src/randomnumbers/RandomNumbers.java
@@ -1,27 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * A program that prints 2 random numbers and checks which one is greater
+ * @author Neele
  */
 package randomnumbers;
 import java.util.*;
-/**
- *
- * @author Neele
- */
+
 public class RandomNumbers {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
         for (int i = 0; i < 25; i++)
         {
+            /**
+             * Generate 2 random numbers between 0 and 100
+             */
             Random rand = new Random();
             int rand1 = rand.nextInt(100);
             int rand2 = rand.nextInt(100);
             
+            /**
+             * if-else statement to check which of the two nmbers is larger
+             */
             if (rand1 >= rand2)
             {
                 System.out.printf("Random1 '%d' is greater or equal to Random2 '%d'",rand1,rand2);
