2fcc5e9e144b5651e31afdf3eb4ada11e5cdb4ec
Peter Tucker <ptucker@whitworth.edu>
2015-01-08 08:02:58 -0800

    Added feedback

--- PrimeNumbers/src/primenumbers/PrimeNumbers.java
+++ PrimeNumbers/src/primenumbers/PrimeNumbers.java
@@ -5,7 +5,9 @@
 package primenumbers;
 import java.util.Scanner;
 
+//PT: need javadoc here. -3
 public class PrimeNumbers {
+    //PT: need javadoc here
     public static void main(String[] args) {
         /**
          * Prompt user for max range (when program should stop)
@@ -21,6 +23,9 @@ public class PrimeNumbers {
             /**
              * Inner For-loop tests which numbers are prime
              */
+             //PT: Why start at 1? Everything is divisible by one.
+             //    Also, you can end at x<i, rather than x<=i.
+             //    These changes would make the if statement cleaner. -2
              for (int x = 1; x <= i ; x++){
                  if (i % x == 0 && i != x && x !=1) // if i is divisible by x but i and x are not the same, loop breaks
                     break;
@@ -29,6 +34,8 @@ public class PrimeNumbers {
                   * Important part: test if i is odd, i and x are the same and i % x is 0
                   * If so, the program prints the numbers
                   */   
+                  //PT: Better to make this happen outside the loop, so that the if
+                  //    statement only happens once
                  if (i % 2 != 0 && i == x && i % x == 0) // 
                      {
                      System.out.printf("The Number %d is a prime number", i);
